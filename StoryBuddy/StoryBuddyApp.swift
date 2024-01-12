c8e14a91769fb85283f2f05283dfd5917dfd1e9a
Aaron Butler <aaronkbutler@icloud.com>
2024-01-12 15:06:02 -0500

    Initial Commit

---
+++ StoryBuddy/StoryBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryBuddyApp.swift
+//  StoryBuddy
+//
+//  Created by Aaron Butler on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct StoryBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
